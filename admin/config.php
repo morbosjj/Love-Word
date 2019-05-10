241080ad6e71bb95a5f209b22498c935e153d62a
J Morbos <morbosjessiejohn@gmail.com>
2019-05-10 12:10:17 +0800

    delete first
    a

--- admin/config.php
+++
@@ -1,4 +0,0 @@
-<?php 
-	include ('../server.php');
-	$db = mysqli_connect('localhost', 'root','','loveword') or die('Unable to connect');
-?>
--- audit.php
+++
@@ -1,8 +0,0 @@
-<?php include('server.php'); ?>	
-<?php 
-	if (isset($_POST['login'])) {
-		$_SESSION['username'] = $username;
-		$insertaud = mysqli_query("INSERT INTO audit(username, logintime)
-			VALUES('$username');");
-	}
-?>
